184967f18f8258d2f945aa7c5f2228ecb3d99a62
mibu119 <horang296@gmail.com>
2023-04-21 16:54:51 +0900

    🌱 230421

---
+++ LV. 0/05 - 정수 부분.js
@@ -0,0 +1,8 @@
+// 정수 부분
+function solution(flo) {
+  var answer = 0;
+
+  answer = Math.floor(flo);
+
+  return answer;
+}
---
+++ LV. 0/06 - 문자열로 변환.js
@@ -0,0 +1,8 @@
+// 문자열로 변환
+function solution(n) {
+  var answer = "";
+
+  answer = `${n}`;
+
+  return answer;
+}
