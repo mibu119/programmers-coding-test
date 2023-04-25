8ad14d91060e94129a9da813baed4608285a7ca7
mibu <114740795+mibu119@users.noreply.github.com>
2023-04-25 20:37:18 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,13 @@
+function solution(sides) {
+    var answer = 0;
+    
+    sides = sides.sort((a,b) => a-b)
+    
+    if(sides[0] + sides[1] > sides[2]){
+        answer = 1
+    } else {
+        answer = 2
+    }
+    
+    return answer;
+}
