27c3e07f8d8898e26df87c14fde5bec99bb7b6ec
mibu <114740795+mibu119@users.noreply.github.com>
2023-05-03 16:40:01 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,15 @@
+function solution(numbers, n) {
+    var answer = 0;
+    let sum = 0;
+    
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i]
+        
+        if(sum > n){
+            answer = sum
+            break
+        }
+    }
+    
+    return answer;
+}
