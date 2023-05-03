798886a0610546d91414b7bd9309cb7bce4ab290
mibu <114740795+mibu119@users.noreply.github.com>
2023-05-03 17:12:16 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.68 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,10 @@
+function solution(n, control) {
+    const controller = {
+        w:1,
+        s:-1,
+        d:10,
+        a:-10
+    }
+ 
+    return [...control].reduce((acc, cur)=> acc + controller[cur], n);
+}
