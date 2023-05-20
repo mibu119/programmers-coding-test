838f0978ce24b343a2737dbef9539f7cba7c6855
mibu <114740795+mibu119@users.noreply.github.com>
2023-05-20 11:55:13 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,8 @@
+function solution(arr, idx) {
+//     var answer = 0;
+    
+//     let num = arr.findIndex((e, i) => e !== 0 && i >= idx)
+    
+//     return answer = num ? num : -1
+    return arr.indexOf(1,idx)
+}
