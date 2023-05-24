04dcd87fb54104c00ac17b292e651cded4f758ee
mibu <114740795+mibu119@users.noreply.github.com>
2023-05-24 11:44:47 +0900

    [unrated] Title: qr code, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.js
+++ 프로그래머스/unrated/181903. qr code/qr code.js
@@ -1,6 +1,6 @@
 function solution(q, r, code) {
     var answer = '';
-    let codeArr = code.split('')
+    let codeArr = [...code]
     
     for(let i = 0; i < codeArr.length; i++){
         if(i % q === r){
