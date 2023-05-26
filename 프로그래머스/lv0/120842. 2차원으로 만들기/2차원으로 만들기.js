02d40ae153a22410baa18925b8cc4cc179f77fbe
mibu <114740795+mibu119@users.noreply.github.com>
2023-05-26 10:18:05 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,6 @@
 function solution(num_list, n) {
     var answer = [];
     
-    let nl = num_list.length / n
     for(let i = 0; i < num_list.length; i+=n){
         answer.push(num_list.slice(i, n + i))
     }
