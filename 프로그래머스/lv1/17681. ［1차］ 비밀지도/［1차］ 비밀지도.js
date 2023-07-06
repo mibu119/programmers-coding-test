3d0417630bb17ede946a4ccb064e0f1caefe18c2
mibu <114740795+mibu119@users.noreply.github.com>
2023-07-06 12:13:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,38 @@
+function solution(n, arr1, arr2) {
+    let answer = [];
+    
+    function zerofill_1(value, fillCount){
+        let result = value;
+        if(typeof value === "number") result = value.toString()
+        return result.padStart(fillCount, '0')
+    }
+    
+    function change(arr){
+        return  arr.map((el) => el.toString(2)).map((e) => 
+            e.length !== n ? zerofill_1(e, n) : e
+        )
+    }
+    
+    let change01 = change(arr1)
+    let change02 = change(arr2)
+    
+    for(let i = 0; i < change01.length; i++){
+       let e = Number(change01[i]) + Number(change02[i])
+       answer.push(zerofill_1(e, n))
+    }
+    
+    answer = answer.map(e => e.toString()).map(e => {
+        let str = ``
+        for(const num of e){
+          if(num !== `0`){
+              str += `#`
+          }  else {
+              str += ` `
+          }
+        }
+        return str
+    })
+    
+    
+    return answer;
+}
