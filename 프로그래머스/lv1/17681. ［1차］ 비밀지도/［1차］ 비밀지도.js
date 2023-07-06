3b5b5103bfec928c557e2d999b0f01d5f3f2a611
mibu <114740795+mibu119@users.noreply.github.com>
2023-07-06 12:14:52 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,38 +1,46 @@
+// function solution(n, arr1, arr2) {
+//     let answer = [];
+    
+//     function zerofill_1(value, fillCount){
+//         let result = value;
+//         if(typeof value === "number") result = value.toString()
+//         return result.padStart(fillCount, '0')
+//     }
+    
+//     function change(arr){
+//         return  arr.map((el) => el.toString(2)).map((e) => 
+//             e.length !== n ? zerofill_1(e, n) : e
+//         )
+//     }
+    
+//     let change01 = change(arr1)
+//     let change02 = change(arr2)
+    
+//     for(let i = 0; i < change01.length; i++){
+//        let e = Number(change01[i]) + Number(change02[i])
+//        answer.push(zerofill_1(e, n))
+//     }
+    
+//     answer = answer.map(e => e.toString()).map(e => {
+//         let str = ``
+//         for(const num of e){
+//           if(num !== `0`){
+//               str += `#`
+//           }  else {
+//               str += ` `
+//           }
+//         }
+//         return str
+//     })
+    
+    
+//     return answer;
+// }
+
 function solution(n, arr1, arr2) {
-    let answer = [];
-    
-    function zerofill_1(value, fillCount){
-        let result = value;
-        if(typeof value === "number") result = value.toString()
-        return result.padStart(fillCount, '0')
-    }
-    
-    function change(arr){
-        return  arr.map((el) => el.toString(2)).map((e) => 
-            e.length !== n ? zerofill_1(e, n) : e
-        )
-    }
-    
-    let change01 = change(arr1)
-    let change02 = change(arr2)
-    
-    for(let i = 0; i < change01.length; i++){
-       let e = Number(change01[i]) + Number(change02[i])
-       answer.push(zerofill_1(e, n))
-    }
-    
-    answer = answer.map(e => e.toString()).map(e => {
-        let str = ``
-        for(const num of e){
-          if(num !== `0`){
-              str += `#`
-          }  else {
-              str += ` `
-          }
-        }
-        return str
-    })
-    
-    
-    return answer;
+    return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
+}
+
+const addZero = (n, s) => {
+    return '0'.repeat(n - s.length) + s;
 }
