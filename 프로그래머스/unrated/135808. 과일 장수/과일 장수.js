7d7898ac418e6c6fa052867271359e80574c46ee
mibu <114740795+mibu119@users.noreply.github.com>
2023-07-18 22:40:41 +0900

    [unrated] Title: 과일 장수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,26 +1,26 @@
-// function solution(k, m, score) {
-//     score.sort((a,b) => b - a)
-//     let result = []
-//     for(let i = 0; i < score.length; i += m){
-//         result.push(score.slice(i, i+m))
-//     }
-//     result = result.filter((arr) => arr.length === m).map((e) => Math.min(...e) * m).reduce((a,b) => (a+b))
-    
-//     return result;
-// }
-
 function solution(k, m, score) {
-    score.sort((a, b) => b - a);
-    let sum = 0;
-    let min = Infinity;
-
-    for (let i = 0; i < score.length; i++) {
-        min = Math.min(min, score[i]);
-        if ((i + 1) % m === 0) {
-            sum += min * m;
-            min = Infinity;
-        }
+    score.sort((a,b) => b - a)
+    let result = []
+    for(let i = 0; i < score.length; i += m){
+        result.push(score.slice(i, i+m))
     }
-
-    return sum;
+    result = result.filter((arr) => arr.length === m).map((e) => Math.min(...e) * m).reduce((a,b) => (a+b), 0)
+    
+    return result;
 }
+
+// function solution(k, m, score) {
+//     score.sort((a, b) => b - a);
+//     let sum = 0;
+//     let min = Infinity;
+
+//     for (let i = 0; i < score.length; i++) {
+//         min = Math.min(min, score[i]);
+//         if ((i + 1) % m === 0) {
+//             sum += min * m;
+//             min = Infinity;
+//         }
+//     }
+
+//     return sum;
+// }
