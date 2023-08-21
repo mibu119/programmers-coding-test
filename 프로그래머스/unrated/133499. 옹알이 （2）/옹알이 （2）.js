c0fac7423a2508d7221518868bdec2977c811554
mibu <114740795+mibu119@users.noreply.github.com>
2023-08-21 18:43:13 +0900

    [unrated] Title: 옹알이 (2), Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,7 +1,6 @@
 function solution(babbling) {
   let answer = 0;
 
-  // forEach() 메서드로 babbling을 하나씩 확인합니다.
   for (let word of babbling) {
     if (/(aya|ye|woo|ma)\1+/g.test(word)) continue;
     if (/^(aya|ye|woo|ma)+$/g.test(word)) {
