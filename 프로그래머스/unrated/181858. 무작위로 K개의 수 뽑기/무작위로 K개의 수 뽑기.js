f630ba45bbcd820cca548c70d1a7990e9d6068db
mibu <114740795+mibu119@users.noreply.github.com>
2023-08-26 23:04:43 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 0.81 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -0,0 +1,19 @@
+function solution(arr, k) {
+    var answer = [];
+    
+    for(let i = 0; i < arr.length ; i++){
+        if(!answer.includes(arr[i])){
+            answer.push(arr[i])
+            if(answer.length === k){
+               break
+                
+        }     
+    }
+    }
+    
+    while(answer.length < k){
+        answer.push(-1)
+    }
+    
+    return answer;
+}
