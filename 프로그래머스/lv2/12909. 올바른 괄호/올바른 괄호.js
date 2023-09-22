f69c285fb9fc7f330270ee8c0b116f604f84dbac
mibu <114740795+mibu119@users.noreply.github.com>
2023-09-22 13:48:57 +0900

    [level 2] Title: 올바른 괄호, Time: 4.85 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,15 +1,25 @@
-function solution(s) {
-  let count = 0;
-  for (let i = 0; i < s.length; i++) {
-    const char = s[i];
-    if (char === '(') {
-      count++;
-    } else {
-      count--;
-    }
-    if (count < 0) {
-      return false;
-    }
-  }
-  return count === 0;
+// function solution(s) {
+//   let count = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     const char = s[i];
+//     if (char === '(') {
+//       count++;
+//     } else {
+//       count--;
+//     }
+//     if (count < 0) {
+//       return false;
+//     }
+//   }
+//   return count === 0;
+// }
+
+function solution(s){
+    const answer = [];
+
+    [...s].forEach(string => 
+        (string === ')' && answer[answer.length - 1] === '(') ? answer.pop() : answer.push(string)
+    )    
+    
+    return answer.length === 0;
 }
